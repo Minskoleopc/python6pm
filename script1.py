82714badba2ea230b8a466cccf5b7f3539f38c5d
unknown <Minskoleopc>
2024-02-13 17:50:51 +0530

    added

---
+++ script1.py
@@ -0,0 +1,40 @@
+x = 10
+print(x)
+x = 900
+print(x)
+
+# Arithmetic operation
+
+a = 10 
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+q = 8 
+r = 4
+
+print(q+r)
+print(q-r)
+print(q*r)
+print(q/r)
+print(q%r)
+
+# 10 pairs ??  ----- 50 lines
+
+
+def Calculator(x,y):
+    print(x+y)
+    print(x-y)
+    print(x*y)
+    print(x/y)
+    print(x%y)
+Calculator(12,4)
+Calculator(8,4)
+
+
+
+
