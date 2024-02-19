0bdc699899fc4cc7cf714c872b6d798d38ec9bb6
unknown <Minskoleopc>
2024-02-19 20:55:47 +0530

    added

---
+++ script7.py
@@ -0,0 +1,78 @@
+
+names = ["sarika","poorva","amit"]
+names2 = names
+names2[0] = "raj"
+print(names)
+print(names2)
+
+# program 2
+# append()
+
+fruits = ["apple","mango","banana"]
+fruits.append("grapes")
+print(fruits)
+
+
+# program 3
+# pop()
+# pop(index)
+# remove()
+
+#                0            1           2       3
+vegetables = ["cabbage","cauliflower","tomato","potato"]
+# vegetables.pop()
+# print(vegetables)
+# vegetables.pop(1)
+# print(vegetables)
+vegetables.remove("tomato")
+print(vegetables)
+# append() , pop(index) or without index or remove()
+
+
+# program 3
+animals = ["tiger",'lion',"rabbit","panthar","tiger","lion"]
+# animals.clear()
+# print(animals)
+q1  = animals.count('tiger')
+print(q1)
+
+# program 4
+city = ["pune","mumbai","banglore","kolkata"]
+city.reverse()
+print(city)
+# program 5
+#             0         1            2      3
+country = ["india","bangladesh","srilanka","japan"]
+country.insert(2,"china")
+print(country)
+
+# program 6
+district = ["chandrapur","wardha","nagpur"]
+district.sort()
+print(district)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
